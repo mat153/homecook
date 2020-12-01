32fb4a00964c867ca395b613c9e4b46738a94a2b
Mateusz <mateuszgruszeczka@interia.pl>
2020-12-01 12:07:27 +0100

    Add new files

---
+++ src/main/java/com/mat/homecook/HomecookApplication.java
@@ -0,0 +1,13 @@
+package com.mat.homecook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mat/homecook/HomecookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mat.homecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
